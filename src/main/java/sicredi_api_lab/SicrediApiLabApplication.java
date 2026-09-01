ccf7ffdce52ad768009d09e1a3bc13045a5d99ba
junin <juninhoguimar@gmail.com>
2026-09-01 17:06:49 -0300

    sicredi-01: EStrutura inicial

---
+++ src/main/java/sicredi_api_lab/SicrediApiLabApplication.java
@@ -0,0 +1,13 @@
+package sicredi_api_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrediApiLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicrediApiLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sicredi_api_lab/SicrediApiLabApplicationTests.java
@@ -0,0 +1,13 @@
+package sicredi_api_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediApiLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
