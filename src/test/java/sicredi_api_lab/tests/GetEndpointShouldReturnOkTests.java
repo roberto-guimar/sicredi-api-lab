7d06c1a7eb60e4cb7cb88b0711cba811098cb246
junin <juninhoguimar@gmail.com>
2026-09-01 18:14:22 -0300

    sicredi-apis: Ajustado estrutura de testes de APIs

--- src/test/java/sicredi_api_lab/tests/GetEndpointShouldReturnOkTests.java
+++ src/test/java/sicredi_api_lab/tests/GetEndpointShouldReturnOkTests.java
@@ -1,10 +1,10 @@
-package sicredi_api_lab;
+package sicredi_api_lab.tests;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SicrediApiLabApplicationTests {
+class GetEndpointShouldReturnOkTests {
 
 	@Test
 	void contextLoads() {
