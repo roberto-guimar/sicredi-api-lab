3f2fb13fa2c8ffcfea575bcda8fe90719b76c2b5
junin <juninhoguimar@gmail.com>
2026-09-02 12:17:12 -0300

    sicredi-apis: Ajuste para tornar a classe StatusTest imutável e mais simples

--- src/test/java/sicredi_api_lab/domain/model/StatusTest.java
+++ src/test/java/sicredi_api_lab/domain/model/StatusTest.java
@@ -1,6 +1,5 @@
 package sicredi_api_lab.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -10,12 +9,6 @@ public class StatusTest {
     private String method;
 
     public StatusTest() {
-    }
-
-    @JsonCreator
-    public StatusTest(String status, String method) {
-        this.status = status;
-        this.method = method;
     }
 
     public String getStatus() {
